785d85507263627fe3557ac2106a12e441a72a81
Ujjwal Saxena <ujjwal.saxena1606@gmail.com>
2026-09-10 11:31:32 +0530

    Time: 10 ms (80.93%) | Memory: 46.9 MB (57.60%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -6,44 +6,29 @@ class Solution {
         int k = p.length();
         int l = 0;
         int r = 0;
-
         int[] freqp = new int[128];
         int[] freqs = new int[128];
 
         if (p.length() > s.length()) {
             return list;
         }
-
-        // Create the first window
         while (r < k) {
             freqp[p.charAt(r)]++;
             freqs[s.charAt(r)]++;
             r++;
         }
-
-        // Check first window
         if (Arrays.equals(freqp, freqs)) {
             list.add(l);
         }
-
-        // Slide the window
         while (r < s.length()) {
-
-            // Add new character
             freqs[s.charAt(r)]++;
-
-            // Remove old character
             freqs[s.charAt(l)]--;
-
             l++;
             r++;
-
-            // Check current window
             if (Arrays.equals(freqp, freqs)) {
                 list.add(l);
             }
         }
-
         return list;
     }
 }
