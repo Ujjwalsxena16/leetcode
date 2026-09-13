88ef91cd58b684cad6f6c91c3d74a9a9481f115c
Ujjwal Saxena <ujjwal.saxena1606@gmail.com>
2026-09-13 19:46:27 +0530

    Time: 1 ms (99.84%) | Memory: 69.3 MB (27.97%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -4,18 +4,13 @@ class Solution {
         int l =0;
         int sum = 0;
              for (int r = 0; r < nums.length; r++) {
-
             sum += nums[r];
-
             while (sum >= target) {
-
                 min = Math.min(min, r - l + 1);
-
                 sum -= nums[l];
                 l++;
             }
         }
-
       return min == Integer.MAX_VALUE ? 0 : min;
     }
 }
